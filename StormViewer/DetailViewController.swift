1cfd657a2e108a5addcbda3b7e0bec7b74746acc
igibliss00 <igibliss00@gmail.com>
2020-06-28 20:03:28 -0400

    Add the view count feature for each post. Saves/load the view count into/from UserDefault

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var viewCountLabel: UILabel!
+    
     var selectedImage: String?
     var row: Int = 0
     var total: Int = 0
+    var viewCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +25,17 @@ class DetailViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
-        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
+        }
+        
+        let defaults = UserDefaults.standard
+        if let selectedImage = selectedImage {
+            viewCount = defaults.integer(forKey: selectedImage)
+            viewCount += 1
+            viewCountLabel.text = String(viewCount)
+        } else {
+            print("Could not load the view count")
         }
     }
     
@@ -36,6 +47,13 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+        
+        let d = UserDefaults.standard
+        if let selectedImage = selectedImage {
+            d.set(viewCount, forKey: selectedImage)
+        } else {
+            print("Could not save the view count")
+        }
     }
     
     @objc func shareTapped() {
@@ -47,5 +65,22 @@ class DetailViewController: UIViewController {
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
+    
+//    func saveViewCount() {
+//        let jsonEncoder = JSONEncoder()
+//        if let savedData = try? jsonEncoder.encode(viewCount) {
+//            let defaults = UserDefaults.standard
+//            if let selectedImage = selectedImage {
+//                defaults.set(savedData, forKey: selectedImage)
+//            }
+//        } else {
+//            print("Failed to save people")
+//        }
+//    }
+//
+//    func loadViewCount() {
+//        let defaults = UserDefaults.standard
+//
+//    }
 }
 
